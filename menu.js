0594ccc41c115255118559d6b7e80f2a5fc26e57
bwiche <bpwchd@umsystem.edu>
2021-04-13 18:09:45 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,34 @@
 "use strict";
 
-var  menuNav = document.getElementById("menu_nav");
+var menuNav = document.getElementById('menu_nav');
 
-var menuTrigger = document.getElementById("menu_trigger");
-function menutoggle() {
-if (menuNav.style.display === "flex") {
-  menuNav.style.display = none;
-  menuTrigger.style.border ="4px solid purple";
-}else {menuNav.style.display = 'flex';}
-  console.log()"The menu has been toggled!";
+var menuTrigger = document.getElementById('menu_trigger');
+
+var headshot = document.getElementById('headshotImage');
+
+
+
+
+function menuToggle() {
+  if (menuNav.style.display === "flex") {
+    menuNav.style.display = "none";
+    menuTrigger.style.border = "4px solid black"
+    headshot.src = "Headshot.jpg";
+    headshot.style.opacity = '0.5';
+  } else {
+    menuNav.style.display = "flex";
+    menuTrigger.style.border = "4px solid purple";
+    headshot.src = "Headshot.jpg";
+    headshot.style.opacity = '1';
+  }
+  console.log("The menu has been toggled!")
 }
 
+menuTrigger.onclick = menuToggle;
 
-menuTrigger.onclick=menutoggle;
+window.onresize = function() {
+  if (window.innerWidth >= 600) {
+    menuNav.removeAttribute("style");
+    menuTrigger.removeAttribute("style");
+  } ;
+}

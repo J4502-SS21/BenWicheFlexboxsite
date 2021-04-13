65c9332bb0b3663fdef062fa1950068a303acdcc
bwiche <bpwchd@umsystem.edu>
2021-04-13 16:36:00 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,12 +3,12 @@
 var  menuNav = document.getElementById("menu_nav");
 
 var menuTrigger = document.getElementById("menu_trigger");
-
-
-
-menuTrigger.onclick=function() {
+function menutoggle() {
 if (menuNav.style.display === "flex") {
   menuNav.style.display = none;
 }else {menuNav.style.display = 'flex';}
   console.log()"The menu has been toggled!";
 }
+
+
+menuTrigger.onclick=menutoggle;

f0beaa3d4e429d4652af81ade1cb7ef526b46b23
bwiche <bpwchd@umsystem.edu>
2021-04-13 16:31:02 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -8,9 +8,7 @@ var menuTrigger = document.getElementById("menu_trigger");
 
 menuTrigger.onclick=function() {
 if (menuNav.style.display === "flex") {
-  menuNav.style.display = none;}
-}
-else if (menuNav.style.display = none){
-  menuNav.style.display ="flex";
-}
+  menuNav.style.display = none;
+}else {menuNav.style.display = 'flex';}
+  console.log()"The menu has been toggled!";
 }

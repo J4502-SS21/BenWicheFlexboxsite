419a82408a676ec6517e50a67a4e1d9ff9e3ce8e
bwiche <bpwchd@umsystem.edu>
2021-04-13 16:26:10 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -7,5 +7,10 @@ var menuTrigger = document.getElementById("menu_trigger");
 
 
 menuTrigger.onclick=function() {
-console.log("Sup");
+if (menuNav.style.display === "flex") {
+  menuNav.style.display = none;}
+}
+else if (menuNav.style.display = none){
+  menuNav.style.display ="flex";
+}
 }
